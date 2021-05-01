5ce98710e66a060d441d3071c349e1a89fdd28e9
sebastian <shiux@pop-os.localdomain>
2021-04-30 23:01:06 -0500

    fix bug in css of navbary and fix bug in _document file

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -4,7 +4,19 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="es">
-        <Head></Head>
+        <Head>
+          <meta
+            name="description"
+            content="Portafolio de Jairo Sebastian Gil Madrid tambien me puedes encontrar como roku_js o rokujs, aqui muestro varios de mis projectos, desde el desarrollo de videojuegos hasta el desarrollo de paginas web"
+          />
+          <meta name="author" content="Jairo Sebastian" />
+          <link rel="icon" href="/img/logo.svg" type="image/svg+xml" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body className="my-body-class">
           <Main />
           <NextScript />
--- pages/projects/index.jsx
+++ pages/projects/index.jsx
@@ -1,22 +1,29 @@
 import React from "react";
 import Head from "next/head";
-import Axios from "axios";
+import Error from "next/error";
+import fetch from "isomorphic-unfetch";
 
 import Layout from "../../components/Layout/Layout";
 import Portfolio from "../../components/Portfolio/Portfolio";
 
-export const getStaticProps = async () => {
-  const {
-    data: { data },
-  } = await Axios.get(`${process.env.API}api/projects/`);
+export const getServerSideProps = async () => {
+  const res = await fetch(`${process.env.API}api/projects/`);
+  const errorCode = res.ok ? false : res.statusCode;
+  const { data } = await res.json();
+
   return {
     props: {
       projects: data,
+      error: errorCode,
     },
   };
 };
 
-const projects = ({ projects }) => {
+const projects = ({ projects, error }) => {
+  if (error) {
+    return <Error statusCode={error} />;
+  }
+
   return (
     <Layout>
       <Head>
