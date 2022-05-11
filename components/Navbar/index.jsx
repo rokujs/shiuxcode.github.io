02f6033ae226c09492f3a831824dc753f21dec7a
rokujs <shiuxcode@gmail.com>
2022-05-10 20:06:50 -0500

    Fix semantix navbar

--- components/Navbar/index.jsx
+++ components/Navbar/index.jsx
@@ -9,26 +9,26 @@ import style from './styles.module.css'
 
 const Navbar = () => {
   return (
-    <div className={style.container}>
-      <div className={`${style.github} ${style.item}`}>
+    <ul className={style.container}>
+      <li className={`${style.github} ${style.item}`}>
         <a href="https://github.com/rokujs" target="_blank" rel="noreferrer">
           <FontAwesomeIcon icon={faGithubSquare} className={style.icon} />
           <span className={style.animation_icon}>Github</span>
         </a>
-      </div>
-      <div className={`${style.linkedIn} ${style.item}`}>
+      </li>
+      <li className={`${style.linkedIn} ${style.item}`}>
         <a href="https://www.linkedin.com/in/rokujs/" target="_blank" rel="noreferrer">
           <FontAwesomeIcon icon={faLinkedin} className={style.icon} />
           <span className={style.animation_icon}>LinkedIn</span>
         </a>
-      </div>
-      <div className={`${style.twitter} ${style.item}`}>
+      </li>
+      <li className={`${style.twitter} ${style.item}`}>
         <a href="https://twitter.com/rokujs/" target="_blank" rel="noreferrer">
           <FontAwesomeIcon icon={faTwitterSquare} className={style.icon} />
           <span className={style.animation_icon}>Twitter</span>
         </a>
-      </div>
-    </div>
+      </li>
+    </ul>
   )
 }
 
